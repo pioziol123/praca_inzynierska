8b585a0f1630afffbbeb1150a309e0cd5b0677c2
Wojciech Kalinski <wojciech.kalinski@ticos-systems.ch>
2020-04-25 17:21:38 +0200

    Flask env na beanstalku

--- backend/application.py
+++ backend/application.py
@@ -1,12 +1,5 @@
-import datetime
-
-from flask import Flask, request, abort, jsonify, url_for
-from flask_jwt_extended import JWTManager, create_access_token
-from app import db
-from app.models.user import Users, Keywords
-
-from app import app
+from app import application
 import config
 
 if __name__ == '__main__':
-    app.run(debug = config.APP_DEBUG)
+    application.run(debug = config.APP_DEBUG)
