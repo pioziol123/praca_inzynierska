1d3abf038ada8d2e9bf127d6fee20f8a7dbf5d1b
Wojciech Kalinski <wojciech.kalinski@ticos-systems.ch>
2020-07-04 19:19:48 +0200

    Fix na detekcji

--- backend/app/word_analyzer.py
+++ backend/app/word_analyzer.py
@@ -9,15 +9,20 @@ from sklearn.metrics.pairwise import linear_kernel
 from rake_nltk import Rake
 
 def write_data(dataset, y, words):
-    df = pd.DataFrame([(k, v[0][0], v[0][1]) for k, v in dataset.items()],
+    df = pd.DataFrame(dataset,
                    columns=['Kategoria', 'Slowa'])
     df.head()
     df['Key_words'] = words
 
-    count = CountVectorizer()
-    count_matrix = count.fit_transform(df['bag_of_words'])
+    try:
+        count = CountVectorizer()
+        if (df['Slowa']) is None:
+            return '--'
+        count_matrix = count.fit_transform(df['Slowa'])
 
+    except ValueError:
+        return ' '
     # generating the cosine similarity matrix
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
-
+    cosine_sim = dataset[0]
     return cosine_sim
